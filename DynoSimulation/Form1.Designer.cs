e48cecaa5dfd5c2ba8295cb63ba49d844bf2b701
Vernon Williams <voxenn@gmail.com>
2018-10-07 21:00:13 -0500

    First commit

---
+++ DynoSimulation/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DynoSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(124, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(551, 288);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Horsepower";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(681, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Torque";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(369, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "RPM (x1000)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(600, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Run Dyno";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Stage 1",
+            "Stage 2"});
+            this.comboBox1.Location = new System.Drawing.Point(124, 373);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(84, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label4.Location = new System.Drawing.Point(238, 404);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Stock";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(369, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 7;
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label6.Location = new System.Drawing.Point(505, 404);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DynoSimulation/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//This is a simulated graph. The numbers do not accurately reflect an actual dyno.
+namespace DynoSimulation
+{
+    public partial class Form1 : Form
+    {
+        
+        //Initialize variables to use for creating the graph
+        Bitmap drawArea;
+        double acceleration, force, distanceTime;
+        double J, k1, k2;
+        double revs, rpm, rpmPrevious;
+        double time, timePrevious;
+        double torque, torqueFinal, torqueNew;
+        double velocity;
+        Graphics graph;        
+        Pen stockLine = new Pen(Color.Blue, 1);
+        Pen stage1Line = new Pen(Color.Red, 1);
+        Pen stage2Line = new Pen(Color.Green, 1);
+        static Random random = new Random();
+        string stageSelection;
+
+        public void initVar()
+        {
+            acceleration = 0;
+            force = 100;
+            distanceTime = 0.05;
+            J = 0.3;
+            k1 = 0.1; 
+            k2 = 0.001;
+            revs = 0;
+            rpm = 0;
+            rpmPrevious = 0;
+            time = 0;
+            timePrevious = 0;
+            torque = 0;
+            torqueFinal = 0;
+            torqueNew = 0;
+            velocity = 0;
+        }        
+
+        private void dynoStart()
+        {
+            //If we've been on the dyno for more than 8 seconds, stop the dyno
+            if (time > 5 && force > 0)
+            {
+                force -= 1;
+            }        
+            //Torque 
+            torque = force * k1;
+            torqueFinal = k2 * Math.Pow(velocity, 2);
+            torqueNew = torque - torqueFinal;
+            //Acceleration is the change in velocity divided by the change in time
+            acceleration = torqueNew / J;
+            velocity = velocity + acceleration * distanceTime;            
+        }
+
+        private void dynoUpdate(Pen pen, double RPM)
+        {
+            //Draw the next iteration of the dyno curve
+            drawLine(pen, timePrevious * 40, 300 - 0.2 * rpmPrevious, time * 40, 300 - 0.2 * RPM);
+            rpmPrevious = RPM;
+            timePrevious = time;
+            time = time + distanceTime;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            graph = Graphics.FromImage(drawArea);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dynoStart();
+
+            //Simulate the power drops for the curve
+            if (time < 4)
+            {
+                revs = (random.NextDouble() *(28 - 26) + 26);
+            }
+            else if (time >= 4 && time < 9)
+            {
+                revs = (random.NextDouble() * (28 - 27) + 27);
+            }
+            else
+            {
+                revs = (random.NextDouble() * (30 - 28) + 28);
+            }
+            //Calculate the simulated RPM
+            rpm = (revs* velocity) / Math.PI;
+            dynoUpdate(stockLine, rpm);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            dynoStart();
+            //Simulate the power drops for the curve
+            if (time < 4)
+            {
+                revs = (random.NextDouble() * (38 - 36) + 36);
+            }
+            else if (time >= 4 && time < 9)
+            {
+                revs = (random.NextDouble() * (38 - 37) + 37);
+            }
+            else
+            {
+                revs = (random.NextDouble() * (40 - 38) + 38);
+            }
+            //Calculate the simulated RPM
+            rpm = (revs * velocity) / Math.PI;
+            dynoUpdate(stage1Line, rpm);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            dynoStart();
+            //Simulate the power drops for the curve
+            if (time < 4)
+            {
+                revs = (random.NextDouble() * (44 - 43) + 43);
+            }
+            else if (time >= 4 && time < 9)
+            {
+                revs = (random.NextDouble() * (45 - 44) + 44);
+            }
+            else
+            {
+                revs = (random.NextDouble() * (46 - 45) + 45);
+            }
+            //Calculate the simulated RPM
+            rpm = (revs * velocity) / Math.PI;
+            dynoUpdate(stage2Line, rpm);
+        }
+
+        private void drawLine(Pen pen, double x1, double y1, double x2, double y2)
+        {
+            graph.DrawLine(pen, Convert.ToInt32(x1), Convert.ToInt32(y1), Convert.ToInt32(x2), Convert.ToInt32(y2));
+            pictureBox1.Image = drawArea;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initVar();
+            //Clear the graph
+            graph.Clear(Color.WhiteSmoke);
+            //Determines how fast to draw the graph
+            timer1.Interval = (int)(distanceTime * 500);
+            //Begin the timer
+            timer1.Enabled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            stageSelection = comboBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Disable the first line
+            timer1.Enabled = false;
+            initVar();
+            switch(stageSelection)
+            {
+                case "Stage 1":
+                    if (timer3.Enabled == true)
+                    {
+                        timer3.Enabled = false;
+                    }
+                    label5.Text = "Stage 1";
+                    timer2.Interval = (int)(distanceTime * 500);
+                    timer2.Enabled = true;
+                    break;
+                case "Stage 2":
+                    if (timer2.Enabled == true)
+                    {
+                        timer2.Enabled = false;
+                    }
+                    label6.Text = "Stage 2";
+                    timer3.Interval = (int)(distanceTime * 500);
+                    timer3.Enabled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
